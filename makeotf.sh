b0d3899c74e4e07d2777c6418c45697bea058abb
Alexander Misel <7542439+AlexanderMisel@users.noreply.github.com>
2018-12-16 19:44:09 +0800

    Patch for deepin

--- makeotf.sh
+++ makeotf.sh
@@ -3,10 +3,6 @@ makeotf -f c0648bt_.pfb
 makeotf -f c0632bt_.pfb -b
 makeotf -f c0633bt_.pfb -i -b
 makeotf -f c0649bt_.pfb -i
-makeotf -f cour.pfa
-makeotf -f courb.pfa  -b
-makeotf -f courbi.pfa -i -b
-makeotf -f couri.pfa  -i
 makeotf -f c0419bt_.pfb
 makeotf -f c0583bt_.pfb -b
 makeotf -f c0611bt_.pfb -i -b
@@ -21,21 +17,32 @@ makeotf -f c059036l.pfb -i -b
 makeotf -f c059033l.pfb -i
 makeotf -f a010013l.pfb
 makeotf -f a010015l.pfb -b
+makeotf -f a010033l.pfb -i
+makeotf -f a010035l.pfb -i -b   
 makeotf -f c059013l.pfb
 makeotf -f c059016l.pfb -b
 makeotf -f b018012l.pfb
 makeotf -f b018015l.pfb -b
+makeotf -f b018032l.pfb -b
+makeotf -f b018035l.pfb -i -b
 makeotf -f n019003l.pfb
 makeotf -f n019004l.pfb -b
 makeotf -f n019024l.pfb -i -b
 makeotf -f n019023l.pfb -i
 makeotf -f n019043l.pfb
 makeotf -f n019044l.pfb -b
+makeotf -f n019063l.pfb -i
+makeotf -f n019064l.pfb -i -b
 makeotf -f n021003l.pfb
+makeotf -f n021004l.pfb -b
+makeotf -f n021023l.pfb -i
+makeotf -f n021024l.pfb -i -b
 makeotf -f n022003l.pfb
 makeotf -f n022004l.pfb -b
 makeotf -f n022024l.pfb -i -b
 makeotf -f n022023l.pfb -i
 makeotf -f p052003l.pfb
 makeotf -f p052004l.pfb -b
+makeotf -f p052023l.pfb -i
+makeotf -f p052024l.pfb -i -b
 makeotf -f z003034l.pfb -i
